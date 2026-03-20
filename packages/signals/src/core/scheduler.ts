1487425e1a84fe936aed5b964b1bb4bfb4623a2b
@casey_baggz_omni <casey.baggz@omnifederal.com>
2026-03-20 13:27:18 -0500

    fix(signals): support complex subscriptions preventing diamond problem

--- packages/signals/src/core/scheduler.ts
+++ packages/signals/src/core/scheduler.ts
@@ -13,25 +13,54 @@ export function popObserver() {
 }
 
 // Batching State
-export let batchDepth = 0
-export const batchedObservers = new Set<Observer>()
 
-export function batch<T>(fn: () => T): T {
-  batchDepth++
+export const batchedObservers: Set<Observer> = new Set<Observer>()
+let isBatching: boolean = false
+
+/**
+ * ## Batching Signal Setters
+ *
+ * Allows multiple signal setters to be batched together, ensuring they run in a
+ * single update. This is useful when you want to update multiple signals at once,
+ * without triggering multiple updates.
+ *
+ * | Parameters | Type | Description |
+ * |------------|------|-------------|
+ * | `callback` | `() => void` | The callback to batch any signal updates into a single update. |
+ */
+export function batch<T>(fn: () => T): void {
+  if (isBatching) {
+    fn()
+    return
+  }
+  isBatching = true
   try {
-    return fn()
+    fn()
   } finally {
-    batchDepth--
-    if (batchDepth === 0) {
-      flush()
-    }
+    isBatching = false
+    flush()
   }
 }
 
-function flush() {
-  const observersToRun = Array.from(batchedObservers)
+export function schedule(observer: Observer): void {
+  if (isBatching) {
+    batchedObservers.add(observer)
+  } else {
+    batchedObservers.add(observer)
+    flush()
+  }
+}
+
+function flush(): void {
+  if (batchedObservers.size === 0) return
+
+  const queue = Array.from(batchedObservers)
   batchedObservers.clear()
-  for (const observer of observersToRun) {
+  // Sort ascending by depth. Closest to the root (Signal) runs first.
+  queue.sort((a, b) => a.depth - b.depth)
+
+  // Execute safely top-down
+  for (const observer of queue) {
     observer.execute()
   }
 }
